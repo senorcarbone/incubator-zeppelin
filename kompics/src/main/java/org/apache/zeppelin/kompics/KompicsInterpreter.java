8c56f00f8a241eeaf4082ffb1fb3f49519c71da8
Paris Carbone <parisc@kth.se>
2016-06-15 19:24:24 +0200

    strip project and update basic kompics interpreter

--- kompics/src/main/java/org/apache/zeppelin/kompics/KompicsInterpreter.java
+++ kompics/src/main/java/org/apache/zeppelin/kompics/KompicsInterpreter.java
@@ -23,7 +23,20 @@ import org.apache.zeppelin.interpreter.InterpreterResult;
 import org.apache.zeppelin.interpreter.InterpreterResult.Code;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.Console;
+import scala.Some;
+import scala.runtime.AbstractFunction0;
+import scala.tools.nsc.Settings;
+import scala.tools.nsc.interpreter.IMain;
+import scala.tools.nsc.interpreter.Results;
+import scala.tools.nsc.settings.MutableSettings;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
@@ -32,44 +45,146 @@ import java.util.Properties;
  * Interpreter for Kompics (http://kompics.sics.se/)
  */
 public class KompicsInterpreter extends Interpreter {
-	
+
 	Logger logger = LoggerFactory.getLogger(KompicsInterpreter.class);
+	private ByteArrayOutputStream out;
 
 	static {
 		Interpreter.register("kompics", "kompics", KompicsInterpreter.class.getName());
 	}
 
+	private IMain imain;
+
 
 	public KompicsInterpreter(Properties property) {
 		super(property);
 	}
-	
+
 	@Override
 	public void open() {
+		out = new ByteArrayOutputStream();
+		imain = new IMain(createSettings());
+		resetInterpreter();
+	}
 
+	private void resetInterpreter() {
+		System.setOut(new PrintStream(out));
+		out.reset();
+		imain.reset();
+		Results.Result res = imain.interpret(
+				"import se.sics.kompics.sl._\n" +
+						"import se.sics.kompics.{ Start, Kompics, KompicsEvent }");
+
+		if (getResultCode(res) == Code.ERROR) {
+			throw new RuntimeException("Failed to initialize the kompics interpreter");
+		}
 	}
 
 	@Override
 	public void close() {
+		try {
+			imain.close();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	private List<File> classPath(ClassLoader cl) {
+		List<File> paths = new LinkedList<File>();
+		if (cl == null) {
+			return paths;
+		}
+
+		if (cl instanceof URLClassLoader) {
+			URLClassLoader ucl = (URLClassLoader) cl;
+			URL[] urls = ucl.getURLs();
+			if (urls != null) {
+				for (URL url : urls) {
+					paths.add(new File(url.getFile()));
+				}
+			}
+		}
+		return paths;
+	}
+
+	private List<File> currentClassPath() {
+		List<File> paths = classPath(Thread.currentThread().getContextClassLoader());
+		String[] cps = System.getProperty("java.class.path").split(File.pathSeparator);
+		if (cps != null) {
+			for (String cp : cps) {
+				paths.add(new File(cp));
+			}
+		}
+		return paths;
+	}
+
+	private Settings createSettings() {
+		URL[] urls = getClassloaderUrls();
+		Settings settings = new Settings();
+
+		// set classpath
+		MutableSettings.PathSetting pathSettings = settings.classpath();
+		String classpath = "";
+		List<File> paths = currentClassPath();
+		for (File f : paths) {
+			if (classpath.length() > 0) {
+				classpath += File.pathSeparator;
+			}
+			classpath += f.getAbsolutePath();
+		}
+
+		if (urls != null) {
+			for (URL u : urls) {
+				if (classpath.length() > 0) {
+					classpath += File.pathSeparator;
+				}
+				classpath += u.getFile();
+			}
+		}
+
+		pathSettings.v_$eq(classpath);
+		settings.scala$tools$nsc$settings$ScalaSettings$_setter_$classpath_$eq(pathSettings);
+		settings.explicitParentLoader_$eq(new Some<ClassLoader>(Thread.currentThread()
+				.getContextClassLoader()));
+		MutableSettings.BooleanSetting b = (MutableSettings.BooleanSetting) settings.usejavacp();
+		b.v_$eq(true);
+		settings.scala$tools$nsc$settings$StandardScalaSettings$_setter_$usejavacp_$eq(b);
+
+		return settings;
 	}
 
 	@Override
-	public InterpreterResult interpret(String line, InterpreterContext context) {
-		if (line == null || line.trim().length() == 0) {
+	public InterpreterResult interpret(final String codeStr, InterpreterContext context) {
+		
+		resetInterpreter();
+		
+		if (codeStr == null || codeStr.trim().length() == 0) {
 			return new InterpreterResult(Code.SUCCESS);
 		}
+		
+		Results.Result res = Console.withOut(
+				System.out,
+				new AbstractFunction0<Results.Result>() {
+					@Override
+					public Results.Result apply() {
+						return imain.interpret(codeStr);
+					}
+				});
 
-		InterpreterResult result = interpret(line.split("\n"), context);
-		return result;
+		return new InterpreterResult(getResultCode(res), out.toString());
 	}
 
-	public InterpreterResult interpret(String[] lines, InterpreterContext context) {
-		
-		return new InterpreterResult(Code.SUCCESS, "Hello Kompics!");
-		
+	private Code getResultCode(scala.tools.nsc.interpreter.Results.Result r) {
+		if (r instanceof scala.tools.nsc.interpreter.Results.Success$) {
+			return Code.SUCCESS;
+		} else if (r instanceof scala.tools.nsc.interpreter.Results.Incomplete$) {
+			return Code.INCOMPLETE;
+		} else {
+			return Code.ERROR;
+		}
 	}
-	
+
 	@Override
 	public void cancel(InterpreterContext context) {
 	}
--- kompics/src/test/java/org/apache/zeppelin/kompics/KompicsInterpreterTest.java
+++ kompics/src/test/java/org/apache/zeppelin/kompics/KompicsInterpreterTest.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,93 @@
  */
 package org.apache.zeppelin.kompics;
 
+import org.apache.zeppelin.interpreter.InterpreterContext;
+import org.apache.zeppelin.interpreter.InterpreterResult;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class KompicsInterpreterTest {
 
+	private static KompicsInterpreter kompicspreter;
+	private static InterpreterContext context;
+
+	@BeforeClass
+	public static void setUp() {
+		Properties p = new Properties();
+		kompicspreter = new KompicsInterpreter(p);
+		try {
+			kompicspreter.open();
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		context = new InterpreterContext(null, null, null, null, null, null, null, null, null, null, null);
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		kompicspreter.close();
+		kompicspreter.destroy();
+	}
+
+	@Test
+	public void testNextLineInvocation() {
+		assertEquals(InterpreterResult.Code.SUCCESS, kompicspreter.interpret("\"123\"\n.toInt", context).code());
+	}
+
+	@Test
+	public void testNextLineComments() {
+		assertEquals(InterpreterResult.Code.SUCCESS, kompicspreter.interpret("\"123\"\n/*comment here\n*/.toInt", context).code());
+	}
+
+	@Test
+	public void testNextLineCompanionObject() {
+		String code = "class Counter {\nvar value: Long = 0\n}\n // comment\n\n object Counter {\n def apply(x: Long) = new Counter()\n}";
+		assertEquals(InterpreterResult.Code.SUCCESS, kompicspreter.interpret(code, context).code());
+	}
+
+	@Test
+	public void testSimpleStatement() {
+		kompicspreter.interpret("val a=1", context);
+		InterpreterResult result = kompicspreter.interpret("print(a)", context);
+		assertEquals("1", result.message());
+	}
+
+	@Test
+	public void testSimpleStatementWithSystemOutput() {
+		kompicspreter.interpret("val a=1", context);
+		InterpreterResult result = kompicspreter.interpret("System.out.print(a)", context);
+		assertEquals("1", result.message());
+	}
+
+	@Test
+	public void testKompicsComponents() {
+
+		InterpreterResult result = kompicspreter.interpret(
+				"class HelloWorldC extends ComponentDefinition {\n" +
+				"    ctrl uponEvent {\n" +
+				"        case _: Start => handle {\n" +
+				"            println(\"Hello World!\");\n" +
+				"            Kompics.asyncShutdown();\n" +
+				"        }\n" +
+				"    }\n" +
+				"}\n" +
+				"\n" +
+				"Kompics.createAndStart(classOf[HelloWorldC])", context);
+
+		assertEquals(InterpreterResult.Code.SUCCESS, result.code());
+//
+//		String[] expectedCounts = {"(to,2)", "(be,2)", "(or,1)", "(not,1)"};
+//		Arrays.sort(expectedCounts);
+//
+//		String[] counts = result.message().split("\n");
+//		Arrays.sort(counts);
+//
+//		assertArrayEquals(expectedCounts, counts);
+	}
 }
